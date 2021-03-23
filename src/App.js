7894ba7edf910a172d9e7793f92b505ff1373b57
jang_eun_ju <dmswn0261@ajou.ac.kr>
2021-03-24 03:14:00 +0900

    TopBar finish

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import React, {Component} from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
+import MainPage from './MainPage/MainPage.js';
+import NoticeList from './NoticeList/NoticeList.js';
+import StudyList from './StudyList/StudyList.js';
+import TopBar from './TopBar/TopBar.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        APP.JS
-      </header>
-      <div className="page">
-        page
-      </div>
+      <BrowserRouter>
+        <Route exact path='/' component = {MainPage}/>
+        <Route exact path='/StudyList' component = {StudyList}/>
+        <Route exact path='/NoticeList' component = {NoticeList}/>
+      </BrowserRouter>
     </div>
   );
 }
--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -1,6 +1,10 @@
+import React, {Component} from 'react';
+import TopBar from '../TopBar/TopBar.js';
+
 function MainPage() {
     return (
       <div className="MainPage">
+        <TopBar/>
         main page
       </div>
     );
