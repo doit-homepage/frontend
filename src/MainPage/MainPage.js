8f10670975facb3bb94d62397d5c72cbc2de9dbc
jang_eun_ju <dmswn0261@ajou.ac.kr>
2021-03-25 02:02:52 +0900

    mainpage add

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -11,21 +11,42 @@ class MainPage extends Component{
       ObjectArray[3] = "./do-it 사진3.png";
       function changeImage(){
         document.getElementById("MainPage_image").src=ObjectArray[n++];
-        
         if(n>ObjectArray.length-1){
           n=1;
         }
+        setTimeout(changeImage,7000);
       }
-    return (
+      setTimeout(changeImage,0);
+      var k=1
+      function changeBack(){
+        document.getElementById("MainPage_back").src=ObjectArray[k++];
+        if(k>ObjectArray.length-1){
+          k=1;
+        }
+        setTimeout(changeBack,7000);
+      }
+      setTimeout(changeBack,0);
+      return (
       <div className="MainPage">
-        <div>
-          <img onLoad = {((event) => setInterval(changeImage, 3000))}  id = "MainPage_image" src = "./do-it 사진.jpg"></img>
-          <script>
-            
-          </script>        
+        <div className="MainPage_up">
+          <img id = "MainPage_image" src = "./do-it 사진.jpg"></img> 
+          <img id = "MainPage_back" src = "./do-it 사진.jpg"></img> 
+          <div className="MainPage_btns"> 
+            <button id="MainPage_1" className="MainPage_btn"></button> 
+            <button id="MainPage_1" className="MainPage_btn"></button> 
+            <button id="MainPage_1" className="MainPage_btn"></button> 
+          </div>  
         </div>
-      </div>
-      
+        <div className="MainPage_down">
+          <div className="MainPage_Notice">
+            <p id="MainPage_notice">공지사항</p>
+            <div id="MainPage_notice1">  
+              <img className="MainPage_noticeImg" src="./Do-it 로고.jpg"></img>
+            </div>
+            
+          </div>
+        </div>
+      </div>    
     );
     }
   }
