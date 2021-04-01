eb785e599205f2de976a7c20d8b2056119660413
gaeunU <dbrkdmsyge12@naver.com>
2021-04-01 19:47:36 +0900

    commit message

--- src/NoticeDetail/NoticeDetail.js
+++ src/NoticeDetail/NoticeDetail.js
@@ -1,46 +1,50 @@
 import React, {Component} from 'react';
 import TopBar from '../TopBar/TopBar.js';
 import styles from './NoticeDetail.css';
+import axios from 'axios'
 
 function NoticeDetail() {
+  function test(){
+    axios.post('/api/info',{}).then((res)=>{
+      console.log(res)
+    })
+
     return (
       <div className="NoticeDetail">
-        <TopBar/>
-        <div className="text">
+        <div className="NoticeDetail_text">
           공지사항
           </div>
           <hr width = "1020"></hr>
-          <div className="Box_top">
-          <h1>[글머리] 공지사항입니다 </h1>   
-          <h2>12 ❤️</h2> 
+          <div className="NoticeDetailBox_top">
+          <p className="NoticeDetail_notice">[글머리] 공지사항입니다 </p>   
+          <p className="NoticeDetail_Like">12 ❤️</p> 
           </div>
   
-          <div className="writerinfo">
+          <div className="NoticeDetail_writerinfo">
             정성원    2020-03-07    조회수210
             </div>
-          <p div className='add'>첨부파일: 참고자료.xlsx</p>
-        <div className="Box_bottom"></div>
+          <p div className='NoticeDetail_add'>첨부파일: 참고자료.xlsx</p>
+        <div className="NoticeDetailBox_bottom"></div>
         <hr width = "1020"></hr>
         
-        <h3>이전글</h3>
-        <h4>        [글머리] 이전글 제목</h4>
-       
+        <p className="NoticeDetail_next">이전글</p>
+        <p classNape="NoticeDetail_bottomtext">[글머리] 이전글 제목</p>
 
         <hr width = "1020"></hr>
 
         
-        <h3>다음</h3>
-        <h4>        [글머리] 다음 제목</h4>
+        <p className="NoticeDetail_next">다음</p>
+        <p className="NoticeDetail_bottomtext">[글머리] 다음 제목</p>
         
 
         <hr width = "1020"></hr>
 
       
-        <button className ="List">목록</button>
+        <button className ="NoticeDetail_List">목록</button>
         
         
       </div>
     );
   }
-  
+}
 export default NoticeDetail;
--- src/StudyDetail/StudyDetail.js
+++ src/StudyDetail/StudyDetail.js
@@ -5,41 +5,41 @@ import styles from './StudyDetail.css';
 function StudyDetail() {
   return (
     <div className="StudyDetail">
-      <TopBar/>
-      <div className="text">
+      
+      <div className="StudyDetail_text">
         스터디 / 프로젝트
         </div>
         <hr width = "1020"></hr>
-        <div className="Box_top">
-        <h1>[글머리] 웹스터디 모집 </h1>   
-        <h2>모집인원: 5명</h2> 
-        <h5>2020-03-08 - 2020-03-18</h5>
+        <div className="StudyDetail_Box_top">
+        <p className = "StudyDetail_text">[글머리] 웹스터디 모집 </p>   
+        <p className="StudyDetail_studentnum">모집인원: 5명</p> 
+        <p className="SudyDetail_date">2020-03-08 - 2020-03-18</p>
         </div>
 
-        <div className="writerinfo">
+        <div className="StudyDetail_writerinfo">
           정성원    2020-03-07    조회수210
           </div>
-        <p div className='add'>첨부파일: 참고자료.xlsx</p>
-      <div className="Box_bottom"></div>
+        <p div className='StudyDetail_add'>첨부파일: 참고자료.xlsx</p>
+      <div className="StudyDetail_Box_bottom"></div>
       <hr width = "1020"></hr>
       
-      <h3>이전글</h3>
-      <h4>        [글머리] 이전글 제목</h4>
-     
+      <p clsssName="StudyDetail_next">이전글</p>
+      <p className="StudyDetail_bottomtext"> [글머리] 이전글 제목</p>
+      
 
       <hr width = "1020"></hr>
 
       
-      <h3>다음</h3>
-      <h4>        [글머리] 다음 제목</h4>
+      <p clsssName="StudyDetail_next">다음</p>
+      <p className="StudyDetail_bottomtext">[글머리] 다음 제목</p>
       
 
       <hr width = "1020"></hr>
 
     
-      <button className ="List">목록</button>
+      <button className ="StudyDetail_List">목록</button>
       
-      
+
     </div>
   );
 }
