7e855154815a101c45e530c5d719e5feb98c05c3
gaeunU <dbrkdmsyge12@naver.com>
2021-04-04 01:27:53 +0900

    dd

--- src/NoticeDetail/NoticeDetail.js
+++ src/NoticeDetail/NoticeDetail.js
@@ -22,19 +22,19 @@ function NoticeDetail() {
       </div>
 
       <div className="NoticeDetail_writerinfo">
-        정성원    2020-03-07    조회수210
+        정성원  &nbsp; 2020-03-07  &nbsp; 조회수210
             </div>
       <p div className='NoticeDetail_add'>첨부파일: 참고자료.xlsx</p>
       <div className="NoticeDetailBox_bottom"></div>
       <hr width="1020"></hr>
 
-      <p className="NoticeDetail_next">이전글</p>
-      <p classNape="NoticeDetail_bottomtext">[글머리] 이전글 제목</p>
+      <p className="NoticeDetail_next">이전글 &nbsp;&nbsp;</p>
+      <p className="NoticeDetail_bottomtext">[글머리] 이전글 제목</p>
 
       <hr width="1020"></hr>
 
 
-      <p className="NoticeDetail_next">다음</p>
+      <p className="NoticeDetail_next">다음 &nbsp;&nbsp;</p>
       <p className="NoticeDetail_bottomtext">[글머리] 다음 제목</p>
 
 
