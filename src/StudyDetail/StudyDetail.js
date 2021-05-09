9becd7abbad9929c24655127bf54d5ebc4117c9f
gaeunU <dbrkdmsyge12@naver.com>
2021-05-10 02:10:12 +0900

    axios

--- src/StudyDetail/StudyDetail.js
+++ src/StudyDetail/StudyDetail.js
@@ -1,48 +1,50 @@
-import React, {Component} from 'react';
-import TopBar from '../TopBar/TopBar.js';
-import styles from './StudyDetail.css';
+import React, { Component } from "react";
+import TopBar from "../TopBar/TopBar.js";
+import styles from "./StudyDetail.css";
 
 function StudyDetail() {
+  const axios = require("axios");
+  axios
+    .get("/index")
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(response);
+    });
+
   return (
     <div className="StudyDetail">
-      
-      <div className="StudyDetail_text">
-        스터디 / 프로젝트
-        </div>
-        <hr width = "1020"></hr>
-        <div className="StudyDetail_Box_top">
-        <p className = "StudyDetail_title">[글머리] 웹스터디 모집 </p>   
-        <p className="StudyDetail_studentnum">모집인원: 5명 &nbsp;</p> 
+      <div className="StudyDetail_text">스터디 / 프로젝트</div>
+      <hr width="1020"></hr>
+      <div className="StudyDetail_Box_top">
+        <p className="StudyDetail_title">[글머리] 웹스터디 모집 </p>
+        <p className="StudyDetail_studentnum">모집인원: 5명 &nbsp;</p>
         <p className="SudyDetail_date">2020-03-08 - 2020-03-18</p>
-        </div>
+      </div>
 
-        <div className="StudyDetail_writerinfo">
-          정성원 &nbsp; 2020-03-07&nbsp; 조회수210
-          </div>
-        <p div className='StudyDetail_add'>첨부파일: 참고자료.xlsx</p>
+      <div className="StudyDetail_writerinfo">
+        정성원 &nbsp; 2020-03-07&nbsp; 조회수210
+      </div>
+      <p div className="StudyDetail_add">
+        첨부파일: 참고자료.xlsx
+      </p>
       <div className="StudyDetail_Box_bottom"></div>
-      <hr width = "1020"></hr>
-       
+      <hr width="1020"></hr>
+
       <p className="StudyDetail_next">이전글 &nbsp;</p>
       <p className="StudyDetail_bottomtext"> [글머리] 이전글 제목</p>
-      
 
-      <hr width = "1020"></hr>
+      <hr width="1020"></hr>
 
-      
       <p className="StudyDetail_next">다음 &nbsp;&nbsp;</p>
       <p className="StudyDetail_bottomtext">[글머리] 다음 제목</p>
-      
 
-      <hr width = "1020"></hr>
+      <hr width="1020"></hr>
 
-    
-      <button className ="StudyDetail_List">목록</button>
-      
-
+      <button className="StudyDetail_List">목록</button>
     </div>
   );
 }
-  
+
 export default StudyDetail;
-  
