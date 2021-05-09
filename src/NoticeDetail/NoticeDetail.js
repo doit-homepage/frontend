2f3a5e8854a01880079320d155eed5a4d20d4258
gaeunU <dbrkdmsyge12@naver.com>
2021-05-10 02:07:09 +0900

    axios-gaeun

--- src/NoticeDetail/NoticeDetail.js
+++ src/NoticeDetail/NoticeDetail.js
@@ -1,20 +1,40 @@
-import React, { Component } from 'react';
-import TopBar from '../TopBar/TopBar.js';
-import './NoticeDetail.css';
-import axios from 'axios'
+import React, { Component } from "react";
+import TopBar from "../TopBar/TopBar.js";
+import "./NoticeDetail.css";
+import axios from "axios";
 
 function NoticeDetail() {
-  function test() {
-    axios.post('/api/info', {}).then((res) => {
-      console.log(res)
+  const axios = require("axios");
+  axios
+    .get("/:id")
+    .then(function (response) {
+      console.log(response);
     })
+    .catch(function (error) {
+      console.log(response);
+    });
+
+  axios
+    .post("/like", {
+      user_id: "userid",
+      notice_id: "noticeid",
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  function test() {
+    axios.post("/api/info", {}).then((res) => {
+      console.log(res);
+    });
   }
-  test()
+  test();
   return (
     <div className="NoticeDetail">
-      <div className="NoticeDetail_text">
-        공지사항
-          </div>
+      <div className="NoticeDetail_text">공지사항</div>
       <hr width="1020"></hr>
       <div className="NoticeDetailBox_top">
         <p className="NoticeDetail_notice">[글머리] 공지사항입니다 </p>
@@ -22,9 +42,11 @@ function NoticeDetail() {
       </div>
 
       <div className="NoticeDetail_writerinfo">
-        정성원  &nbsp; 2020-03-07  &nbsp; 조회수210
-            </div>
-      <p div className='NoticeDetail_add'>첨부파일: 참고자료.xlsx</p>
+        정성원 &nbsp; 2020-03-07 &nbsp; 조회수210
+      </div>
+      <p div className="NoticeDetail_add">
+        첨부파일: 참고자료.xlsx
+      </p>
       <div className="NoticeDetailBox_bottom"></div>
       <hr width="1020"></hr>
 
@@ -33,19 +55,13 @@ function NoticeDetail() {
 
       <hr width="1020"></hr>
 
-
       <p className="NoticeDetail_next">다음 &nbsp;&nbsp;</p>
       <p className="NoticeDetail_bottomtext">[글머리] 다음 제목</p>
 
-
       <hr width="1020"></hr>
 
-
       <button className="NoticeDetail_List">목록</button>
-
-
     </div>
   );
-
 }
 export default NoticeDetail;
