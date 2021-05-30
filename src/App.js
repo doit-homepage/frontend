98a17a4412039329421000d45517ffbb1e36a32a
gaeunU <dbrkdmsyge12@naver.com>
2021-05-31 02:44:02 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import React, {Component} from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import './App.css';
-import MainPage from './MainPage/MainPage.js';
-import NoticeList from './NoticeList/NoticeList.js';
-import StudyList from './StudyList/StudyList.js';
-import TopBar from './TopBar/TopBar.js';
-import StudyDetail from './StudyDetail/StudyDetail.js'
-import Login from './Login/Login.js'
-import Signup from './Signup/Signup.js'
-import NoticeDetail from './NoticeDetail/NoticeDetail'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import MainPage from "./MainPage/MainPage.js";
+import NoticeList from "./NoticeList/NoticeList.js";
+import StudyList from "./StudyList/StudyList.js";
+import TopBar from "./TopBar/TopBar.js";
+import StudyDetail from "./StudyDetail/StudyDetail.js";
+import Login from "./Login/Login.js";
+import Signup from "./Signup/Signup.js";
+import NoticeDetail from "./NoticeDetail/NoticeDetail";
 
 function App(props) {
-  const { getToken, setToken } = props
+  const { getToken, setToken } = props;
   return (
     <div className="App">
       <Router>
-        <div className = "App_header">
-          <TopBar/>
+        <div className="App_header">
+          <TopBar />
         </div>
-        <div className = "App_body">
-          <Route exact path='/' component = {MainPage}/>
-          <Route exact path='/StudyList' component = {StudyList}/>
-          <Route exact path='/NoticeList' component = {NoticeList}/>
-          <Route exact path='/StudyDetail' component = {StudyDetail}/>
-          <Route exact path='/Login'
-            render={() => <Login getToken={getToken} setToken={setToken}/>}
+        <div className="App_body">
+          <Route exact path="/" component={MainPage} />
+          <Route exact path="/StudyList" component={StudyList} />
+          <Route exact path="/NoticeList" component={NoticeList} />
+          <Route exact path="/StudyDetail" component={StudyDetail} />
+          <Route
+            exact
+            path="/Login"
+            render={() => <Login getToken={getToken} setToken={setToken} />}
           />
-          <Route exact path='/Signup' component = {Signup}/>
-          <Route exact path='/NoticeDetail' component = {NoticeDetail}/>
+          <Route exact path="/Signup" component={Signup} />
+          <Route exact path="/NoticeDetail" component={NoticeDetail} />
         </div>
       </Router>
     </div>
--- src/NoticeDetail/NoticeDetail.js
+++ src/NoticeDetail/NoticeDetail.js
@@ -5,54 +5,57 @@ import axios from "axios";
 import { render } from "@testing-library/react";
 
 class NoticeDetail extends React.Component {
-  componentWillMount() {
-    NoticeData = (event) => {
-      axios.get("/api/info/:id").then((res) => {
-        conssole.log(res.info.id);
+  NoticeData = (event) => {
+    axios.get("/api/info/1").then((res) => {
+      console.log(res);
+    });
+  };
+  NoticeLike = (event) => {
+    axios
+      .post("/api/info/like", { user_id: "userid", notice_id: "noticeid" })
+      .then(function () {
+        console.log("좋아요 등록");
+      })
+      .catch(function (error) {
+        console.log("좋아요 등록 실패");
       });
-    };
-
-    NoticeLike = (event) => {
-      axios
-        .post("/api/info/like", { user_id: "userid", notice_id: "noticeid" })
-        .then(function () {
-          console.log("좋아요 등록");
-        })
-        .catch(function (error) {
-          console.log("좋아요 등록 실패");
-        });
-    };
+  };
+  componentWillMount() {
+    this.NoticeData();
   }
+
   render() {
-    <div className="NoticeDetail">
-      <div className="NoticeDetail_text">공지사항</div>
-      <hr width="1020"></hr>
-      <div className="NoticeDetailBox_top">
-        <p className="NoticeDetail_notice">[글머리] 공지사항입니다 </p>
-        <p className="NoticeDetail_Like">12</p>
-        <button onClick={NoticeLike()}>❤️</button>
+    return (
+      <div className="NoticeDetail">
+        <div className="NoticeDetail_text">공지사항</div>
+        <hr width="1020"></hr>
+        <div className="NoticeDetailBox_top">
+          <p className="NoticeDetail_notice">[글머리] 공지사항입니다 </p>
+          <p className="NoticeDetail_Like">12</p>
+          <button onClick={this.NoticeLike()}>❤️</button>
+        </div>
+        <div className="NoticeDetail_writerinfo">
+          정성원 &nbsp; 2020-03-07 &nbsp; 조회수210
+        </div>
+        <p div className="NoticeDetail_add">
+          첨부파일: 참고자료.xlsx
+        </p>
+        <div className="NoticeDetailBox_bottom"></div>
+        <hr width="1020"></hr>
+
+        <p className="NoticeDetail_next">이전글 &nbsp;&nbsp;</p>
+        <p className="NoticeDetail_bottomtext">[글머리] 이전글 제목</p>
+
+        <hr width="1020"></hr>
+
+        <p className="NoticeDetail_next">다음 &nbsp;&nbsp;</p>
+        <p className="NoticeDetail_bottomtext">[글머리] 다음 제목</p>
+
+        <hr width="1020"></hr>
+
+        <button className="NoticeDetail_List">목록</button>
       </div>
-      <div className="NoticeDetail_writerinfo">
-        정성원 &nbsp; 2020-03-07 &nbsp; 조회수210
-      </div>
-      <p div className="NoticeDetail_add">
-        첨부파일: 참고자료.xlsx
-      </p>
-      <div className="NoticeDetailBox_bottom"></div>
-      <hr width="1020"></hr>
-
-      <p className="NoticeDetail_next">이전글 &nbsp;&nbsp;</p>
-      <p className="NoticeDetail_bottomtext">[글머리] 이전글 제목</p>
-
-      <hr width="1020"></hr>
-
-      <p className="NoticeDetail_next">다음 &nbsp;&nbsp;</p>
-      <p className="NoticeDetail_bottomtext">[글머리] 다음 제목</p>
-
-      <hr width="1020"></hr>
-
-      <button className="NoticeDetail_List">목록</button>
-    </div>;
+    );
   }
 }
 export default NoticeDetail;
