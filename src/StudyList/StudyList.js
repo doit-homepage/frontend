a4e1e9eeb7a4e7f8dc9791d7c2f0edaf87783a14
i-nooo <ssfoxs@kakao.com>
2021-06-23 11:59:16 +0900

    스터디/프로젝트 scss 수정
    전반적인 scss 수정
    (신청된 스터디/프로젝트 flexed -> 고정으로 바꿈.)
    
    백엔드 연결 필요 (그전에 신청된 스터디/프로젝트 api 구현 필요)

--- src/StudyList/StudyList.js
+++ src/StudyList/StudyList.js
@@ -4,13 +4,15 @@ import styles from './StudyList.scss';
 import axios from 'axios';
 import { Component } from 'react';
 
+//신청된 스터디 및 프로젝트 api가 없음.
+
 class StudyList extends Component {
   state = {dataList:[]};
   getData(){
     axios.get('/study/list/:1').then((res)=>{
       console.log(res);
       this.setState({dataList: res.data.StudyList})
-      console.log("성공했습니다!!");
+      console.log("(스터디 목록) 성공했습니다!!");
     });
   }
   componentDidMount(){
@@ -18,39 +20,32 @@ class StudyList extends Component {
   }
   render(){
     const {dataList} = this.state;
-
+    console.log(dataList)
   return (
-    <div className={'StudyList'}>
+    <div all>
       <TopBar/>
       <div className = {'ReservedLsit'}>
-        <div className={'NoticeTitle'}>스터디/프로젝트 목록</div>
-        <hr width = "1020" className = {'StudyHR'}></hr>
-        <ul>
-        {dataList.map(data => {
-          <li>
-            <div className={'Study_Card'}>
-              <img
-                //alt="articleImage"
-                className={'Study_Image'}
-                //src="#"
-              />
-              <div className={'Study_content'}>
-                <p className={'Study_bullet'}>[{data.header}]</p>
-                <p className={'Study_title'}>{data.title}</p>
-                <p className={'Study_authorName'}>{data.writer}</p>
-                <p className={'Study_personnel'}>모집인원 : {data.student_num}명</p>
-              </div>
-            </div>
-          </li>
-        })}
-        <hr width="1500" className={'NoticeHR'}></hr>
-        </ul>
-        
-      </div>
+      <div className={'StudyTitle'}>신청한 스터디/프로젝트 목록</div>
+      <hr width = "1020" className = {'TitleHR'}></hr>
       <ul>
-        <div className={'NoticeTitle'}>스터디/프로젝트 목록</div>
-        <hr width="1020" className={'StudyHR'}></hr>
-
+        { dataList.map(data => {
+          return(
+            <li>
+              <div className={'Study_Card'}>
+                <img
+                  //alt="articleImage"
+                  className={'Study_Image'}
+                  //src="#"
+                />
+                <div className={'Study_content'}>
+                  <p className={'Study_bullet'}>[{data.header}]</p>
+                  <p className={'Study_title'}>{data.title}</p>
+                  <p className={'Study_authorName'}>{data.writer}</p>
+                  <p className={'Study_personnel'}>모집인원 : {data.student_num}명</p>
+                </div>
+              </div>
+            </li>
+        )})}
         <li>
         <div className={'Study_Card'}>
             <img
@@ -63,24 +58,8 @@ class StudyList extends Component {
                 <p className={'Study_title'}>웹 스터디 모집</p>
                 <p className={'Study_authorName'}>정성원</p>
                 <p className={'Study_personnel'}>모집인원 : 7명</p>
-              </div>
-          </div>
-        </li>
-        
-        <li>
-        <div className={'Study_Card'}>
-            <img
-              //alt="articleImage"
-              className={'Study_Image'}
-              //src="#"
-            />
-            <div className={'Study_content'}>
-                <p className={'Study_bullet'}>[스터디]</p>
-                <p className={'Study_title'}>웹 스터디 모집</p>
-                <p className={'Study_authorName'}>정성원</p>
-                <p className={'Study_personnel'}>모집인원 : 7명</p>
-              </div>
-          </div>
+            </div>
+        </div>
         </li>
         <li>
         <div className={'Study_Card'}>
@@ -94,9 +73,16 @@ class StudyList extends Component {
                 <p className={'Study_title'}>웹 스터디 모집</p>
                 <p className={'Study_authorName'}>정성원</p>
                 <p className={'Study_personnel'}>모집인원 : 7명</p>
-              </div>
+            </div>
           </div>
         </li>
+      </ul>
+      <hr width="1020" className={'TitleHR2'}></hr>
+      </div>
+      <div className={'recruit'}>
+      <div className={'StudyTitle'}>모집중인 스터디/프로젝트 목록</div>
+      <hr width = "1020" className = {'TitleHR'}></hr>
+      <ul>
         <li>
         <div className={'Study_Card'}>
             <img
@@ -113,6 +99,7 @@ class StudyList extends Component {
           </div>
         </li>
       </ul>
+      </div>
     </div>
   );
 }}
